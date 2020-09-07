665f6f49ec352472378fdc610eadb81a3bd63743
Matija-dev <58312620+Matija-dev@users.noreply.github.com>
2020-09-07 05:41:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var numberA = prompt("Unesi vrednost poluprečnika:");
+var numberB = prompt("Unesi vrednost visine:");
+var numA = parseInt(numberA) * parseInt(numberA)
+var result = numA * parseInt(numberB) * 3.14;
+alert("V= " + result);
+
+
+
+
+
+
+
+
+
+
